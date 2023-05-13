a7da3f2ef515b987e5458c65d400fc334bb864c5
Rahul <rahulghosh@duck.com>
2023-05-13 18:18:57 +0530

    Creat hero and search part

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ collapsibles.forEach((item) =>
         this.classList.toggle("collapse--expanded");
     })
 );
+
+// Script to change the placeholder on large screens
+const searchInput = document.querySelector('.search');
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+
+function handleChange(e) {
+  if (e.matches) {
+    searchInput.placeholder = 'Enter your preferred language';
+  } else {
+    searchInput.placeholder = 'Search';
+  }
+}
+
+handleChange(mediaQuery);
+mediaQuery.addEventListener('change', handleChange);
